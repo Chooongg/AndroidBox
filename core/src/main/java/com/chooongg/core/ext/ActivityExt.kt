b2a11f04b8209fbd9d315b579c729c8a48d7cc96
种耀淮 <chooongg@outlook.com>
2021-10-28 00:05:11 +0800

    .

---
+++ core/src/main/java/com/chooongg/core/ext/ActivityExt.kt
@@ -0,0 +1,2 @@
+package com.chooongg.core.ext
+
--- core/src/main/java/com/chooongg/toolbar/BoxToolbar.kt
+++ core/src/main/java/com/chooongg/toolbar/BoxToolbar.kt
@@ -52,9 +52,6 @@ class BoxToolbar @JvmOverloads constructor(
             if (activity is AppCompatActivity) {
                 activity.setSupportActionBar(this)
             }
-            if (activity is BoxActivity) {
-                activity.initActionBar(this)
-            }
         }
     }
 
