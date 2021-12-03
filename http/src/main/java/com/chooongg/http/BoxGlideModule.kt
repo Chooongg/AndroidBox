7ddd47de8766d34326ae31d81bec95504318756a
Chooongg <chooongg@outlook.com>
2021-12-03 09:42:43 +0800

    .

--- http/src/main/java/com/chooongg/http/BoxGlideModule.kt
+++ http/src/main/java/com/chooongg/http/BoxGlideModule.kt
@@ -1,8 +1,27 @@
 package com.chooongg.http
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import com.chooongg.ext.isAppDebug
 
 @GlideModule(glideName = "BoxGlide")
 class BoxGlideModule : AppGlideModule() {
+    @SuppressLint("CheckResult")
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setLogRequestOrigins(isAppDebug())
+        builder.setLogLevel(Log.INFO)
+        builder.setDefaultRequestOptions {
+            return@setDefaultRequestOptions RequestOptions().apply {
+                placeholder(R.color.divider)
+                fallback(R.color.divider)
+                error(R.color.divider)
+                useAnimationPool(true)
+            }
+        }
+    }
 }
--- http/src/main/java/com/chooongg/http/ext/GlideExt.kt
+++ http/src/main/java/com/chooongg/http/ext/GlideExt.kt
@@ -5,49 +5,34 @@ import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
-import com.bumptech.glide.RequestBuilder
 import com.chooongg.http.BoxGlide
 import java.io.File
 
-fun ImageView.load(bitmap: Bitmap?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(bitmap)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(bitmap: Bitmap?) = let { BoxGlide.with(this).load(bitmap) }
 
-fun ImageView.load(drawable: Drawable?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(drawable)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(drawable: Drawable?) = let { BoxGlide.with(this).load(drawable) }
 
-fun ImageView.load(url: String?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(url)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(url: String?) = let { BoxGlide.with(this).load(url) }
 
-fun ImageView.load(uri: Uri?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(uri)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(uri: Uri?) = let { BoxGlide.with(this).load(uri) }
 
-fun ImageView.load(file: File?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(file)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(file: File?) = let { BoxGlide.with(this).load(file) }
 
-fun ImageView.load(@DrawableRes resId: Int?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(resId)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(@DrawableRes resId: Int?) = let { BoxGlide.with(this).load(resId) }
 
-fun ImageView.load(byte: ByteArray?, block: (RequestBuilder<Drawable>.() -> Unit)? = null) {
-    val builder = BoxGlide.with(this).load(byte)
-    block?.invoke(builder)
-    builder.into(this)
-}
+fun ImageView.load(byte: ByteArray?) = let { BoxGlide.with(this).load(byte) }
 
+
+fun ImageView.loadGif(bitmap: Bitmap?) = let { BoxGlide.with(this).asGif().load(bitmap) }
+
+fun ImageView.loadGif(drawable: Drawable?) = let { BoxGlide.with(this).asGif().load(drawable) }
+
+fun ImageView.loadGif(url: String?) = let { BoxGlide.with(this).asGif().load(url) }
+
+fun ImageView.loadGif(uri: Uri?) = let { BoxGlide.with(this).asGif().load(uri) }
+
+fun ImageView.loadGif(file: File?) = let { BoxGlide.with(this).asGif().load(file) }
+
+fun ImageView.loadGif(@DrawableRes resId: Int?) = let { BoxGlide.with(this).asGif().load(resId) }
+
+fun ImageView.loadGif(byte: ByteArray?) = let { BoxGlide.with(this).asGif().load(byte) }
