99a7564ebb8c8ee27f5b6a305a9f7fd9364b4cf4
Chooongg <chooongg@outlook.com>
2022-01-28 16:28:47 +0800

    .

--- core/src/main/java/com/chooongg/manager/WindowPreferencesManager.kt
+++ core/src/main/java/com/chooongg/manager/WindowPreferencesManager.kt
@@ -1,14 +1,11 @@
 package com.chooongg.manager
 
-import android.R
-import android.annotation.TargetApi
 import android.content.Context
 import android.graphics.Color
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.view.View
 import android.view.Window
-import androidx.annotation.RequiresApi
 import androidx.core.graphics.ColorUtils
 import com.google.android.material.color.MaterialColors
 
@@ -16,13 +13,14 @@ class WindowPreferencesManager(private val context: Context) {
 
     private val isEdgeToEdgeEnabled: Boolean = true
 
+    @Suppress("DEPRECATION")
     fun applyEdgeToEdgePreference(window: Window) {
         val edgeToEdgeEnabled: Boolean = isEdgeToEdgeEnabled
         val statusBarColor: Int = getStatusBarColor(isEdgeToEdgeEnabled)
         val navbarColor: Int = getNavBarColor(isEdgeToEdgeEnabled)
         val lightBackground: Boolean = MaterialColors.isColorLight(
             MaterialColors.getColor(
-                context, R.attr.colorBackground, Color.BLACK
+                context, android.R.attr.colorBackground, Color.BLACK
             )
         )
         val lightStatusBar: Boolean = MaterialColors.isColorLight(statusBarColor)
@@ -45,37 +43,37 @@ class WindowPreferencesManager(private val context: Context) {
         decorView.systemUiVisibility = systemUiVisibility
     }
 
-    @TargetApi(VERSION_CODES.LOLLIPOP)
+    @Suppress("SameParameterValue")
     private fun getStatusBarColor(isEdgeToEdgeEnabled: Boolean): Int {
         if (isEdgeToEdgeEnabled && VERSION.SDK_INT < VERSION_CODES.M) {
             val opaqueStatusBarColor: Int =
-                MaterialColors.getColor(context, R.attr.statusBarColor, Color.BLACK)
+                MaterialColors.getColor(context, android.R.attr.statusBarColor, Color.BLACK)
             return ColorUtils.setAlphaComponent(opaqueStatusBarColor, EDGE_TO_EDGE_BAR_ALPHA)
         }
         if (isEdgeToEdgeEnabled) {
             return Color.TRANSPARENT
         }
-        return MaterialColors.getColor(context, R.attr.statusBarColor, Color.BLACK)
+        return MaterialColors.getColor(context, android.R.attr.statusBarColor, Color.BLACK)
     }
 
-    @TargetApi(VERSION_CODES.LOLLIPOP)
+    @Suppress("SameParameterValue")
     private fun getNavBarColor(isEdgeToEdgeEnabled: Boolean): Int {
         if (isEdgeToEdgeEnabled && VERSION.SDK_INT < VERSION_CODES.O_MR1) {
             val opaqueNavBarColor: Int =
-                MaterialColors.getColor(context, R.attr.navigationBarColor, Color.BLACK)
+                MaterialColors.getColor(context, android.R.attr.navigationBarColor, Color.BLACK)
             return ColorUtils.setAlphaComponent(opaqueNavBarColor, EDGE_TO_EDGE_BAR_ALPHA)
         }
         if (isEdgeToEdgeEnabled) {
             return Color.TRANSPARENT
         }
-        return MaterialColors.getColor(context, R.attr.navigationBarColor, Color.BLACK)
+        return MaterialColors.getColor(context, android.R.attr.navigationBarColor, Color.BLACK)
     }
 
     companion object {
         private const val EDGE_TO_EDGE_BAR_ALPHA: Int = 128
 
-        @RequiresApi(VERSION_CODES.LOLLIPOP)
-        private val EDGE_TO_EDGE_FLAGS: Int =
+        @Suppress("DEPRECATION")
+        private const val EDGE_TO_EDGE_FLAGS: Int =
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 }
